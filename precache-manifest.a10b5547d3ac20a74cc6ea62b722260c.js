5e75e3fcd222b78b09e26c904b201116c67c1da0
İbrahim <eflakkont@gmail.com>
2020-01-08 09:39:37 +0000

    Add files via upload

---
+++ precache-manifest.a10b5547d3ac20a74cc6ea62b722260c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd65307087f289941b5bb0745039d777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88315b719ad5acbc84f6",
+    "url": "/static/css/2.f477f4b5.chunk.css"
+  },
+  {
+    "revision": "74b8ae24024cb7248108",
+    "url": "/static/css/main.1ecfa107.chunk.css"
+  },
+  {
+    "revision": "88315b719ad5acbc84f6",
+    "url": "/static/js/2.e189356c.chunk.js"
+  },
+  {
+    "revision": "1214f0350405497c3c6a2373448e27dd",
+    "url": "/static/js/2.e189356c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74b8ae24024cb7248108",
+    "url": "/static/js/main.7441fa96.chunk.js"
+  },
+  {
+    "revision": "a2eac44eb2622e0d244f",
+    "url": "/static/js/runtime-main.143e16e4.js"
+  }
+]);
